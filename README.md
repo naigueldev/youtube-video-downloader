bf021c59e2af7428510d90d616cb8c724354fc66
Naiguel Santos <naiguelsantosdeveloper@gmail.com>
2022-05-14 03:34:56 -0300

    run script

---
+++ README.md
@@ -0,0 +1,9 @@
+# YouTube Video Downloader
+
+Dependencies to install:
+
+* [youtube-dl](https://github.com/ytdl-org/youtube-dl)
+
+* [python](https://www.python.org/)
+
+* [ffmpeg](https://linuxize.com/post/how-to-install-ffmpeg-on-ubuntu-18-04/)
---
+++ run.rb
@@ -0,0 +1,28 @@
+# https://github.com/arirusso/viddl
+
+require "viddl"
+require "date"
+
+default_folder = "assets/video"
+
+crop_range = [
+  { time_start: 0, time_end: 25 },
+  { time_start: 25, time_end: 30 },
+  { time_start: 40, time_end: 51 },
+]
+
+video_link = "https://www.youtube.com/watch?v=XtroQ8ipXFI"
+
+crop_range.each do |range|
+  date_time_now = DateTime.now.strftime("%Q")
+  output_file_path = "#{default_folder}/#{date_time_now}-#{range[:time_start]}-#{range[:time_end]}"
+
+  options = {
+    start: range[:time_start],
+    end: range[:time_end],
+    output_path: output_file_path,
+  }
+
+  video = Viddl::Video.download(video_link)
+  video.create_clip(options)
+end
